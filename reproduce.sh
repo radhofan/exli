a84a0d49eb1215b8b0c23e6efee11f3b771e180b
Radhofan <radhofanazizi@gmail.com>
2025-03-07 14:20:05 +0700

    ada

--- reproduce.sh
+++ reproduce.sh
@@ -2,17 +2,25 @@
 
 # Set Up
 
+# Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-sudo usermod -aG docker $USER
-newgrp docker
-docker build -t exli .
-docker run --rm exli /bin/bash -c '
-    cd exli/python && \
-    bash exli/python/prepare-conda-env.sh && \
-    source /opt/conda/etc/profile.d/conda.sh && \
-    conda activate exli && \
-    echo "Python environment activated successfully" && \
-    bash exli/python/reproduce.sh
-'
 
+# Add user to the docker group
+sudo usermod -aG docker $USER
+
+# Log out and log back in (or use `su` to reload group permissions)
+# Alternatively, use `sg` to switch to the docker group for the current session
+sg docker -c "
+    docker build -t exli . && \
+    docker run --rm exli /bin/bash -c '
+        cd exli/python && \
+        chmod +x exli/python/prepare-conda-env.sh && \
+        chmod +x exli/python/reproduce.sh && \
+        bash exli/python/prepare-conda-env.sh && \
+        source /opt/conda/etc/profile.d/conda.sh && \
+        conda activate exli && \
+        echo \"Python environment activated successfully\" && \
+        bash exli/python/reproduce.sh
+    '
+"
