0e20ff04312768cfed17bced8218ee48d885dda1
Radhofan <radhofanazizi@gmail.com>
2025-03-07 14:23:06 +0700

    ada

--- reproduce.sh
+++ reproduce.sh
@@ -2,25 +2,27 @@
 
 # Set Up
 
+# Fix permissions for the build context
+sudo chown -R $USER:$USER ./my_mounting_point
+chmod -R 755 ./my_mounting_point
+
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
 # Add user to the docker group
 sudo usermod -aG docker $USER
+newgrp docker
 
-# Log out and log back in (or use `su` to reload group permissions)
-# Alternatively, use `sg` to switch to the docker group for the current session
-sg docker -c "
-    docker build -t exli . && \
-    docker run --rm exli /bin/bash -c '
-        cd exli/python && \
-        chmod +x exli/python/prepare-conda-env.sh && \
-        chmod +x exli/python/reproduce.sh && \
-        bash exli/python/prepare-conda-env.sh && \
-        source /opt/conda/etc/profile.d/conda.sh && \
-        conda activate exli && \
-        echo \"Python environment activated successfully\" && \
-        bash exli/python/reproduce.sh
-    '
-"
+# Build and run the Docker container
+docker build -t exli .
+docker run --rm exli /bin/bash -c '
+    cd exli/python && \
+    chmod +x exli/python/prepare-conda-env.sh && \
+    chmod +x exli/python/reproduce.sh && \
+    bash exli/python/prepare-conda-env.sh && \
+    source /opt/conda/etc/profile.d/conda.sh && \
+    conda activate exli && \
+    echo "Python environment activated successfully" && \
+    bash exli/python/reproduce.sh
+'
