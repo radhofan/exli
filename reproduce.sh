d2a250ef2e3ab6f18d8a57cd14fab603c749ea10
Radhofan <radhofanazizi@gmail.com>
2025-03-07 14:25:08 +0700

    ada

--- reproduce.sh
+++ reproduce.sh
@@ -2,24 +2,16 @@
 
 # Set Up
 
-# Fix permissions for the build context
-sudo chown -R $USER:$USER ./my_mounting_point
-chmod -R 755 ./my_mounting_point
-
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+sudo sh get-docker.sh
 
-# Add user to the docker group
-sudo usermod -aG docker $USER
-newgrp docker
+# Build Docker image
+sudo docker build -t exli .
 
-# Build and run the Docker container
-docker build -t exli .
-docker run --rm exli /bin/bash -c '
+# Run Docker container
+sudo docker run --rm exli /bin/bash -c '
     cd exli/python && \
-    chmod +x exli/python/prepare-conda-env.sh && \
-    chmod +x exli/python/reproduce.sh && \
     bash exli/python/prepare-conda-env.sh && \
     source /opt/conda/etc/profile.d/conda.sh && \
     conda activate exli && \
